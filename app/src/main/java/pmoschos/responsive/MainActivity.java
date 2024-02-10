720400e1311c1c363ddb810def146f58bc1bd886
Panagiotis Moschos <pan.moschos86@gmail.com>
2024-02-11 00:07:04 +0200

    Initial Commit

---
+++ app/src/main/java/pmoschos/responsive/MainActivity.java
@@ -0,0 +1,14 @@
+package pmoschos.responsive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
